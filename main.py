b27de3f6c1c5bb4a2752ccfc8e3a389ccb85b3f1
HIddings25 <829122@mystma.org>
2023-03-29 14:43:24 -0500

    1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMoives(a, b, c, d, e):
-    return a + b + c + d + e
+    return a + " " + b + " " + c + " " + d + " " + e
 
-print(topFiveMoives("Pacific Rim", "movie","top gun", "spiderman" 'END GAME"' ))
+print(topFiveMoives("Pacific Rim", "movie","top gun", "spiderman", 'END GAME"' ))
